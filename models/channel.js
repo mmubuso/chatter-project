3d74950828318ace4d258b91c2c6d0cf36465bfb
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-21 17:42:51 -0400

    create channel model

---
+++ models/channel.js
@@ -0,0 +1,59 @@
+gi// Channel model
+const mongoose = require('./connection.js')
+
+// Creates schema for model
+const ChannelModelSchema = new mongoose.Schema({
+ name: {
+   type: String,
+   default: "Global Chat",
+ },
+ description: {
+   type: String,
+ },
+ color: {
+   type: String
+ },
+ password: {
+   type: String
+  //  required: true
+ }
+  
+
+})
+
+//Name of collection for where channel data will be stored
+const ChannelCollection = mongoose.model('Channel', ChannelModelSchema)
+
+//Get all channels
+function getAllChannels() {
+  return ChannelCollection.find()
+}
+
+//Get one specific channel by Id
+function getOneChannelById(channelId) {
+  return ChannelCollection.findById(channelId)
+}
+
+//Create a Channel
+function createNewChannel(newChannel) {
+  return ChannelCollection.create(channelId)
+}
+
+//Delete a specific channel
+function deleteChannelById(channelId) {
+  return ChannelCollection.findByIdAndDelete(channelId)
+}
+
+//Update a specific channel 
+function updateChannelById(channelId) {
+  return ChannelCollection.findByIdAndDelete(channelId)
+}
+
+//Update a specifci channel by its I
+module.exports = {
+  getAllChannels,
+  getOneChannelById,
+  createNewChannel,
+  deleteChannelById,
+  updateChannelById
+}
