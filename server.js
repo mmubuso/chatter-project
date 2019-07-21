e6de345ebc2ffa260a69849aee186fce47637bf0
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-21 17:53:43 -0400

    add channel router to server

--- server.js
+++ server.js
@@ -1,77 +1,37 @@
-/* 
- * This is the template for a server.js file.  Follow the steps below and read
- * the comments for creating your own (or you can just copy this file).
- */
-
-/* Step 1
- *
- * Import needed packages
- *
- */
+//import express framework
 const express = require('express')
 const app = express()
 
-/* Step 2
- * 
- * import routers from controllers/
- *
- */
-const { templateRouter } = require('./controllers/template.js')
+//import router from controller
+const { channelRouter } = require('./controllers/channel.js')
 
 
-/* Step 3
- *
- * Register middleware...
- */
 
-/* Step 3.a
- * ...to parse the body of the HTTP requests from a URL encoded string 
- */
+//Used to parse the body of the HTTP requests from a URL encoded string 
 app.use(express.urlencoded({extended: true}))
 
-/* Step 3.b 
- *
- * ...to parse the body of the HTTP requests from a JSON string  
- */
+
+ //Used to parse the body of the HTTP requests from a JSON string  
 app.use(express.json())
 
 
-/* Step 3.c
- *
- * use the `./client/build` directory to host static resources such as css and
- * image files 
- */
+
+//use the `./client/build` directory to host static resources such as css and js
 app.use(express.static(`${__dirname}/client/build`))
 
 
-/* Step 4
- *
- * add router for the application to use. The first argument is a prefix to all
- * the paths defined in the router.
- */
-app.use('/api/helloworld', templateRouter)
+//add router for express to used
+app.use('/api/channels', channelRouter)
 
-/* Step 5
- *
- * add catch all route to serve up the built react app for any request not made to our
- * /api/... routes.
- */
+//catches all other get requests that arent aimed at our APIs
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
-/* Step 6
- *
- * Set the port the server is to run on
- *
- * NOTE: keep these lines at the bottom of the file 
- */
+//set the port to be used
 const PORT = process.env.PORT || 3001
 
-/* Step 7
- *
- * Start the server
- */
+//Set por tthat the server will be listening on
 app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`)
 })
