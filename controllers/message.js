d4c398a1d95cf2d0ab373c7c55e64f7b638aa64f
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-21 20:24:16 -0400

    add model and path for message data

---
+++ controllers/message.js
@@ -0,0 +1,55 @@
+//Setup controller for group model
+const express = require('express')
+
+//variable used to access group model methods
+const messageApi = require('../models/message.js')
+
+//Express router
+const messageRouter = express.Router({mergeParams: true})
+
+//Path to get all messages
+messageRouter.get('/', (req, res) => {
+  messageApi.getAllMessages(req.params.groupId )
+    .then(messages => res.send(messages))
+    .catch(err => console.log(err))
+})
+
+//Path to get one message
+messageRouter.get('/:messageId', (req,res) => {
+  messageApi.getOneMessageById(req.params.messageId)
+    .then(message => res.send(message))
+    .catch(err => console.log(err))
+})
+
+//Path to create a new message
+messageRouter.post('/', (req,res) => {
+  messageApi.createNewMessage(req.body, req.params.groupId)
+    .then(createdMessage => res.send(createdMessage))
+    .catch(err => console.log(err))
+})
+
+//Update an individual message
+messageRouter.put('/:messageId', (req,res) =>{
+  messageApi.updateMessageById(req.params.messageId,req.body)
+    .then(updatedMessage => res.send(updatedMessage))
+    .catch(err => console.log(err))
+})
+
+//delete every message 
+messageRouter.delete('/', (req,res) => {
+  messageApi.deleteAllMessages()
+    .then(() => res.send('deleted all messages'))
+    .catch(err => console.log(err))
+})
+
+
+//delete an individual message
+messageRouter.delete('/:messageId', (req,res) => {
+  messageApi.deleteMessageById(req.params.messageId)
+    .then(deletedMessage => res.send(deletedMessage))
+    .catch(err => console.log(err))
+})
+
+module.exports = {
+  messageRouter
+}
---
+++ models/message.js
@@ -0,0 +1,61 @@
+// Channel model
+const mongoose = require('./connection.js')
+
+// Creates schema for model
+const MessageModelSchema = new mongoose.Schema({
+ user: {
+   type: String,
+   default: "Anon"
+ },
+ message: {
+   type: String,
+   default: 'new stuff'
+ },
+ groupId: {
+   type: mongoose.Types.ObjectId
+ }
+})
+
+//Name of collection for where messages data will be stored
+const MessageCollection = mongoose.model('Message', MessageModelSchema)
+
+//Get all messages by groupId 
+function getAllMessages(groupId) {
+  return MessageCollection.find({groupId: groupId})
+}
+
+//Get one specific message by messageId
+function getOneMessageById(messageId) {
+  return MessageCollection.findById(messageId)
+}
+
+//Create a new message by groupId
+function createNewMessage(newMessage, groupId) {
+  newMessage.groupId = groupId
+  return MessageCollection.create(newMessage)
+}
+
+//delete one specific message
+function deleteMessageById(messageId) {
+  return MessageCollection.findByIdAndDelete(messageId)
+}
+
+//delete all messages
+function deleteAllMessages(){
+  return MessageCollection.deleteMany()
+}
+
+//Update a specific messages 
+function updateMessageById(messagesId,newMessages) {
+  return MessageCollection.findByIdAndUpdate(messagesId,newMessages,{new: true})
+}
+
+//Update a specifci Messages by its I
+module.exports = {
+  getAllMessages,
+  getOneMessageById,
+  createNewMessage,
+  deleteMessageById,
+  updateMessageById,
+  deleteAllMessages
+}
