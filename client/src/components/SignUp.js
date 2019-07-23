4b54de1e072242717d27ca49e9958c1e20bedeca
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-23 19:22:24 -0400

    create sign up form to handle creating a user

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
 import {
+    Col,
     Button,
     Form,
     FormGroup,
     Label,
-    Input
 } from 'reactstrap';
 
 export default class SignUp extends Component {
+    //Create the object and call the axios post request to create it
+    //  in the database
+    createObject = (event) => {
+        event.preventDefault()
+        console.log(this.name)
+        this.props.createUser({
+            name: this.name.value,
+            password: this.password.value
+        })
+    }
+
     render() {
         return (
+
             <Form
-                className='container mt-4'
-                >
-                <FormGroup className="mb-2 mr-sm-2 mt-lg-4">
-                    <Label for="name" className="mr-sm-2">Name</Label>
-                    <Input type="name" name="name" id="name" placeholder="Name " />
+                className='col-md-8 col-xs-12'>
+                <h1 className='display-4'>Create An Account</h1>
+                <FormGroup className="row mb-2 mr-sm-0 mt-sm-0 row" >
+                    <Col xs={3} lg={2} >
+                        <Label for="new-name" className="col-xs- mr-sm-0">Name</Label>
+                    </Col>
+                    <Col xs={9} lg={10}>
+                        <input
+                            ref={nameInputElement =>
+                                this.name = nameInputElement
+                            }
+                            className='input form-control'
+                            type="name"
+                            name="name"
+                            id="new-name"
+                            placeholder="Name "
+                             />
+                    </Col>
                 </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="password" className="mr-sm-2">Password</Label>
-                    <Input type="password" name="password" id="password" placeholder="Top secret" />
+                <FormGroup row className="mb-2 mr-sm-0 mb-sm-0 row">
+                    <Col xs={3} lg={2}>
+                        <Label for="new-password" className="mr-sm-0">Password</Label>
+                    </Col>
+                    <Col xs={9} lg={10}>
+                        <input
+                            ref={passwordInputelement =>
+                                this.password = passwordInputelement
+                            }
+                            className='input form-control'
+                            type="password"
+                            name="password"
+                            id="new-password"
+                            placeholder="Top secret"
+                            required />
+                    </Col>
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button onClick={this.createObject} className="mt-4">Submit</Button>
             </Form>
+
         )
     }
 }
