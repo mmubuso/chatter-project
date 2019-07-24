6397c62699968e166e6d128fa7cfaf0eb2a8189a
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-24 08:48:47 -0400

    add path to home component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 import NavBar from './components/NavBar.js';
 import Channel from './components/ChannelContainer.js'
 import Home from './components/Home.js';
+import Group from './components/Group.js'
 
 function App() {
+
+  let HomeComponent = () => <Home />
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Switch>
           {/* Display Channels page */}
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" render={HomeComponent}/>
           <Route path='/channels' component={Channel}/>
           {/* Display groups and message */}
         </Switch>
