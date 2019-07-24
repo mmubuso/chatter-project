9d9c95711bf4bb80570b27923dfa0fb7d25826ee
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-24 19:39:30 -0400

    add method to show only group thats been clicked on

--- client/src/components/GroupsListContainer.js
+++ client/src/components/GroupsListContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
- 
+import Groups from './Groups.js'
+import Messages from './Messages.js'
 
 export default class GroupsListContainer extends Component {
 
@@ -13,6 +14,23 @@ export default class GroupsListContainer extends Component {
             _id: ''
         },
         groupsList: [],
+        activeGroup: {},
+        toggleMessages: false
+    }
+    //Code to run when component mounts
+    componentDidMount() {
+        this.getChannelInformation()
+    }
+
+    //Set active group
+    setAsActiveGroup = (group) => {
+        this.setState({ activeGroup: group })
+        this.showMessages()
+    }
+
+    //toggle messages 
+    showMessages = () => {
+        this.setState({ toggleMessages: true })
     }
 
     //Get all the groups that are associated with this channel
@@ -22,11 +40,6 @@ export default class GroupsListContainer extends Component {
                 let newGroupsList = res.data
                 this.setState({ groupsList: newGroupsList })
             })
-    }
-
-    //Code to run when component mounts
-    componentDidMount() {
-        this.getChannelInformation()
     }
 
     //Get all the information for the current channel
@@ -45,11 +58,22 @@ export default class GroupsListContainer extends Component {
         //Destructure channel object
         let { name, description, color, password, _id } = this.state.channel
 
-        
-        return (
-            <div>
-                <h1>{name}</h1>
 
+        return (
+            <div className='row'>
+                <h1 className='display-3 col-12'>{name}</h1>
+                <Groups
+                    groups={this.state.groupsList}
+                    setActiveGroup={this.setAsActiveGroup} />
+                {
+                    this.state.toggleMessages
+                        ?
+                        <Messages
+                            channel
+                            activeGroup={this.state.activeGroup}
+                        />
+                        : null
+                }
             </div>
         )
     }
