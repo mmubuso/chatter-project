6b56a901a04524b3b387992fa6250c6242ec7ebf
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-01 08:50:27 -0400

    update build files

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b491cdb92b59c96e006196e7f5c81dd4.js"
+  "/precache-manifest.34d9bed92f34b8d0f3a91709a27942b8.js"
 );
 
 self.addEventListener('message', (event) => {
